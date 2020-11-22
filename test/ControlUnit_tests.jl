4d3a47899f05b05955f7ca9907531a63b7c2c318
Kevin Chu <kevin@velexi.com>
2020-11-21 16:59:20 -0800

    Factor test fixtures into their own file.

--- test/ControlUnit_tests.jl
+++ test/ControlUnit_tests.jl
@@ -22,51 +22,7 @@ using Boid
 
 # --- Test fixtures
 
-# ------ Constants
-
-control_url = construct_ipc_url(".", "control-unit-test.zmq")
-ipc_path = control_url[7:end]
-
-# ------ ControlState
-
-mutable struct ControlState <: AbstractControlState
-    is_running::Bool
-    count::Int
-
-    function ControlState()
-        is_running = false
-        count = 0
-        new(is_running, count)
-    end
-end
-
-@enum ControlSignal begin
-    START
-    STOP
-    INCREMENT
-end
-
-function Boid.decode_control_signal(::Type{ControlState}, bytes::Vector{UInt8})
-    return reinterpret(ControlSignal, bytes)[1]
-end
-
-function Boid.get_exception_signal(::Type{ControlState})
-    return "FAILED"
-end
-
-function Boid.process_control_signal!(signal, state::ControlState)
-    if signal == START
-        state.is_running = true
-    elseif signal == STOP
-        state.is_running = false
-    elseif signal == INCREMENT
-        state.count += 1
-    else
-        throw(ArgumentError("Unknown signal"))
-    end
-
-    return "SUCCESS"
-end
+include("fixtures.jl")
 
 # --- Set up/tear down methods
 
