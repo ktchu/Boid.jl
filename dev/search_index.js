59c7223b0875c2d8159cf8b700ac8c512fe54562
Documenter.jl <documenter@juliadocs.github.io>
2022-03-22 06:00:15 +0000

    build based on 0c05b92

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"docs-index/#Index","page":"Index","title":"Index","text":"","category":"section"},{"location":"docs-index/","page":"Index","title":"Index","text":"","category":"page"},{"location":"#Boid.jl","page":"Home","title":"Boid.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Boid.jl","category":"page"}]
+}
